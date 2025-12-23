756fb525510407d3777959d1a9bee2094e7cb171
Suraj Prasad <suraj.prasad@mobcoder.com>
2025-12-23 10:30:22 +0530

    Initial Commit

---
+++ SurajPrasadDemoUITests/SurajPrasadDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SurajPrasadDemoUITestsLaunchTests.swift
+//  SurajPrasadDemoUITests
+//
+//  Created by Mobcoder Technologies Private Limited on 23/12/25.
+//
+
+import XCTest
+
+final class SurajPrasadDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
